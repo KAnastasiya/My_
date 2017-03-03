a71818a911ba8f27a829fefaf648c05efdd8b836
Anastasiia <igoninan87@gmail.com>
2017-03-03 14:36:29 +0200

    Add versions to package.json. Mimimized js

--- webpack.config.js
+++ webpack.config.js
@@ -14,14 +14,14 @@ module.exports = {
 
   plugins: [
     new webpack.NoErrorsPlugin(),
-    // new webpack.optimize.UglifyJsPlugin({
-    //   compress: {
-    //     warnings: false,
-    //     drop_console: true,
-    //     drop_debugger: true,
-    //     unused: true,
-    //     collapse_vars: true,
-    //   },
-    // }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        drop_debugger: true,
+        unused: true,
+        collapse_vars: true,
+      },
+    }),
   ],
 };
